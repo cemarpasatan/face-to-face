1795ce9da4d52e0b007dabd4d9a4e1d748994344
cemarpasatan <cemarpasatan@gmail.com>
2021-06-11 20:10:48 +0300

    ui completed

---
+++ face2face/android/app/src/main/kotlin/com/example/face2face/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.face2face
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ face2face/lib/main.dart
@@ -0,0 +1,58 @@
+
+import 'package:face2face/pages/auth_screen_view.dart';
+import 'package:face2face/provider/profile_provider.dart';
+import 'package:face2face/services/authentication_service.dart';
+import 'package:face2face/widgets/bottom_navbar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+            initialData: null,
+            create: (context) =>
+                context.read<AuthenticationService>().authStateChanges),
+        ChangeNotifierProvider(create: (_) => ProfileProvider(),),
+      ],
+      child: MaterialApp(
+        title: 'Agora Group Video Calling',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.blueAccent,
+        ),
+        home: AuthenticationWrapper(),
+      ),
+    );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      //If the user is successfully Logged-In.
+      return BottomNavbar(firebaseUser);
+    } else {
+      //If the user is not Logged-In.
+      return AuthScreenView();
+    }
+  }
+}
